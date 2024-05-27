0f4e16b50b352c3c67e74af5547f123704f90f3b
wiwsoos <torntan.j@gmail.com>
2024-05-27 20:29:54 +0700

    Change data bindings

---
+++ src/components/Home.js
@@ -0,0 +1,76 @@
+export const Weapons = [
+    {
+      title: "Vandal",
+      tagLink: "/vandal",
+      src: "https://img.4gamers.com.tw/ckfinder-th/image2/auto/2022-05/raze%204.10%20valorant-220527-155155.jpg?versionId=NO7CzAwI.EYhoCPKVVffAYCB8QBWLAJM",
+      flex: 4,
+    },
+    {
+      title: "Phantom",
+      tagLink: "/phantom",
+      src: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fecfc769-eca8-495b-baba-ca4f58048dc7/dfmblxv-2042017b-7178-4bc2-92e5-448e9d826995.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZlY2ZjNzY5LWVjYTgtNDk1Yi1iYWJhLWNhNGY1ODA0OGRjN1wvZGZtYmx4di0yMDQyMDE3Yi03MTc4LTRiYzItOTJlNS00NDhlOWQ4MjY5OTUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.vGdFhxjL8Dek1Z96AXz9RepTyR7t5LfTDCJGOe-EqVo",
+      flex: 4,
+    },
+    {
+      title: "Operator",
+      tagLink: "/operator",
+      src: "https://assets.gamearena.gg/wp-content/uploads/2023/10/26132843/Cypher-mirando.jpg",
+      flex: 4,
+    },
+    {
+      title: "Spectre",
+      tagLink: "/spectre",
+      src: "https://www.si.com/esports/.image/t_share/MjAxMDgzMTI3MjE1NDMzNTkz/reynavalorant.jpg",
+      flex: 4,
+    },
+    {
+      title: "Odin",
+      tagLink: "/odin",
+      src: "https://dotesports.com/wp-content/uploads/2022/08/27152130/valorant-best-agents-beginners-kayo.jpg",
+      flex: 4,
+    },
+  ];
+
+export const Tags = [
+    {
+      tagName: "All Weapons",
+      tagLink: "/allweapon",
+      tagImage: require("../assets/img/allweapon.png"),
+    },
+    {
+      tagName: "Bundles",
+      tagLink: "/bundle",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-prime-vandal-profile-icon.png",
+    },
+    {
+      tagName: "Vandal",
+      tagLink: "/vandal",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-vandal-profile-icon.png",
+    },
+    {
+      tagName: "Phantom",
+      tagLink: "/phantom",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-phantom-profile-icon.png",
+    },
+    {
+      tagName: "Odin",
+      tagLink: "/odin",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-odin-profile-icon.png",
+    },
+    {
+      tagName: "Operator",
+      tagLink: "/operator",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-operator-profile-icon.png",
+    },
+    {
+      tagName: "Spectre",
+      tagLink: "/spectre",
+      tagImage:
+        "https://vgraphs.com/images/weapons/valorant-spectre-profile-icon.png",
+    },
+  ];
